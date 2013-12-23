69654df48d544635912c26a0d53180a3ce8d3e72
Air-T-Mark <air.t.mark@gmail.com>
2013-12-23 18:20:49 +0400

    RFG оптимизация парсера

--- guardy/urls.py
+++ guardy/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 
+from lib.audit import audit
+audit('k-m2.ru')
+
 from django.contrib import admin
 admin.autodiscover()
 
--- lib/audit.py
+++ lib/audit.py
@@ -1,88 +1,87 @@
-import os
 import subprocess
 import re
-from distutils.version import StrictVersion
-import pycurl
-from StringIO import StringIO
-##################### FOR Proof Of Concept ONLY#####################
-
-# f = open('/tmp/nginx_ver','r')
-# text = f.read()
-# text = text.split('<center>')
-# f.close()
-# version = ''
-#
-# for elem in text:
-#     if 'Stable' in elem:
-#         for i in elem.split('<a'):
-#             if 'download' in i:
-#                 version = re.findall('>nginx-(.+)<',i)[0]
-#         break
-# nginx_latest_version = version
-
-##################################
-
-### Snachala privodim vvedenii na saite url k nujnomy vidy
+import urllib
 
 
-
-### Funkciya vizova bash comand
-def use_bash(command,url):
-    bashCommand = command + url
-    input = bashCommand
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+def use_bash(command, url):
+    """
+    bash command caller
+    """
+    bash_command = command + url
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
-    return (output)
+    return output
 
 
-def check_version(product_name):
-    c = pycurl.Curl()
-    url = ''
-    version = 'fuck'
-    output = StringIO()
-    c.setopt(c.WRITEFUNCTION, output.write)
-    if product_name == 'nginx':
-        url = 'nginx.org/en/download.html'
-        c.setopt(c.URL,url)
-        c.perform()
-        c.close()
-        content = output.getvalue()
-        content = content.split('<center>')
-        for elem in content:
-            if 'Stable' in elem:
-                for i in elem.split('<a'):
-                    if 'download' in i:
-                        version = re.findall('>nginx-(.+)<',i)[0]
-                        break
-    elif product_name == 'apache':
-        url = 'httpd.apache.org/download.cgi'
-        c.setopt(c.URL,url)
-        c.perform()
-        c.close()
-        content = output.getvalue()
-        content = content.split('<h1')
+def get_content(url):
+    """
+    get page html
+    """
+    return urllib.urlopen(url).read()
 
-        print content[1]
 
-    print version
-    return version
+class Product(object):
+    """
+    base product object
+    """
+    name = None
+    url = None
+    page = None
+    version = None
+
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        self.page = get_content(url)
+
+
+def get_nginx():
+    """
+    getting nginx object with current version
+    """
+    product = Product(name='nginx', url='nginx.org/en/download.html')
+    content = product.page.split('<center>')
+    for elem in content:
+        if 'Stable' in elem:
+            for i in elem.split('<a'):
+                if 'download' in i:
+                    product.version = re.findall('>nginx-(.+)<', i)[0]
+                    break
+    return product
+
+
+def get_apache():
+    """
+    getting apache object with current version
+    """
+    product = Product(name='apache', url='httpd.apache.org/download.cgi')
+    return  product
+
+
+def get_products():
+    nginx = get_nginx()
+    apache = get_apache()
+    return {
+        nginx.name: nginx,
+        apache.name: apache
+    }
 
 
 def audit(url):
     output = use_bash('whatweb -a 3 ', url + ' --color=never').split(',')
     print output
-    # for line in output:
+    #for line in output:
     #     if 'nginx[' in line:
 	 #    cur_ver = re.findall('nginx\[(.+)\]',line)[0]
 	 #    print cur_ver
 	 #    print nginx_latest_version
     #         if StrictVersion(cur_ver) < StrictVersion(nginx_latest_version):
 	 #        print 'Warning nginx is outdated!'
-		#
-    for elem in output:
 
+    products = get_products()
+    print products
 
-    return
+    return 0
 
 
 audit('k-m2.ru')
